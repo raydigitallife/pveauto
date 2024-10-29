6c29a57b68fb2960ebc602e3a6193b07ec68000d
Ray Huang <raydigitallife@users.noreply.github.com>
2024-10-29 16:21:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install git zsh htop bmon iperf3 vim lshw net-tools lm-sensors sysstat ethtool nfs-kernel-server samba proxmox-auto-install-assistant -y 
+apt install git zsh htop bmon iperf3 vim lshw net-tools lm-sensors sysstat ethtool nfs-kernel-server samba -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 apt dist-upgrade -y && apt full-upgrade -y
